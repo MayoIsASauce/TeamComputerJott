887527a12f655d8beb1cdb9e94f1c627d6fcefb9
Danish Naveed <dmn5413@rit.edu>
2024-10-18 13:22:37 -0400

    Implemented the ElseIfNode.java Parse and convertToJott methods

--- Src/computer/parsernodes/ElseIfNode.java
+++ Src/computer/parsernodes/ElseIfNode.java
@@ -1,31 +1,80 @@
 package computer.parsernodes;
 
 import java.util.ArrayList;
-
 import provided.JottTree;
 import provided.Token;
+import computer.exceptions.ParseException;
 
 public class ElseIfNode implements JottTree {
+
+    private JottTree condition;
+    private JottTree body;
+
+    public ElseIfNode(JottTree condition, JottTree body) {
+        this.condition = condition;
+        this.body = body;
+    }
+
     @Override
     public boolean validateTree() {
-        // TODO Auto-generated method stub
+        // Implement in Phase 3
         return false;
     }
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        return null;
+        return "Elseif[" + condition.convertToJott() + "]{" + body.convertToJott() + "}";
     }
 
-    public static ElseIfNode parse(ArrayList<Token> tokens) {
+    public static ElseIfNode parse(ArrayList<Token> tokens) throws ParseException {
+        // Expecting "Elseif"
+        if (!tokens.get(0).getToken().equals("Elseif")) {
+            throw new ParseException("Expected 'Elseif'");
+        }
+        tokens.remove(0);
 
-        return new ElseIfNode();
+        // Expecting '['
+        if (!tokens.get(0).getToken().equals("[")) {
+            throw new ParseException("Expected '[' after Elseif");
+        }
+        tokens.remove(0);
+
+        // Parse condition
+        JottTree condition = ExprNode.parse(tokens);
+        if (condition == null) {
+            throw new ParseException("Invalid condition in Elseif");
+        }
+
+        // Expecting ']'
+        if (!tokens.get(0).getToken().equals("]")) {
+            throw new ParseException("Expected ']'");
+        }
+        tokens.remove(0);
+
+        // Expecting '{'
+        if (!tokens.get(0).getToken().equals("{")) {
+            throw new ParseException("Expected '{' after condition");
+        }
+        tokens.remove(0);
+
+        // Parse body
+        JottTree body = BodyNode.parse(tokens);
+        if (body == null) {
+            throw new ParseException("Invalid body in Elseif");
+        }
+
+        // Expecting '}'
+        if (!tokens.get(0).getToken().equals("}")) {
+            throw new ParseException("Expected '}' after Elseif body");
+        }
+        tokens.remove(0);
+
+        // Return new ElseIfNode
+        return new ElseIfNode(condition, body);
     }
 
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
-
+        // Phase 4 logic
     }
 }
