66c09202876fff171058026c274abc75350e833a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-10-28 23:02:41 -0400

    symbol table is more complete

--- Src/computer/FunctionInfo.java
+++ Src/computer/FunctionInfo.java
@@ -8,6 +8,17 @@ public class FunctionInfo {
     Types returnType;
     ArrayList<Types> parameterTypes;
 
+    public FunctionInfo(Types returnType, ArrayList<Types> parameterTypes) {
+        this.returnType = returnType;
+        this.parameterTypes = parameterTypes;
+    }
+
+    public FunctionInfo(FunctionInfo other) {
+        this.returnType = other.returnType;
+        // shallow copy
+        this.parameterTypes = new ArrayList<Types>(other.parameterTypes);
+    }
+
     public ArrayList<Types> parameterTypes() {
         return parameterTypes;
     }
--- Src/computer/VariableInfo.java
+++ Src/computer/VariableInfo.java
@@ -6,6 +6,14 @@ public class VariableInfo {
     // NOTE: no need for variable name, stored by name in SymbolTable class
     Types type;
 
+    public VariableInfo(Types type) {
+        this.type = type;
+    }
+
+    public VariableInfo(VariableInfo other) {
+        this.type = other.type;
+    }
+
     public Types type() {
         return type;
     }
