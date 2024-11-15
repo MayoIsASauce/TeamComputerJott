92533d2578c8a56687b8325119634fcdc34035a6
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-11-14 19:30:33 -0500

    exception requires a token to work

--- Src/computer/exceptions/SemanticException.java
+++ Src/computer/exceptions/SemanticException.java
@@ -1,6 +1,11 @@
 package computer.exceptions;
 
+import provided.Token;
+
 public class SemanticException extends Exception
 {
-    public SemanticException(String msg) { super(msg); }
+    public SemanticException(String msg, Token token)
+    {
+        super(msg + "\n" + token.getFilename() + ":" + token.getLineNum());
+    }
 }
