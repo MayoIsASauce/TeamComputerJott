a04c8ac41b833a6c04c5b8392a04590178cbd554
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-12-06 21:38:51 -0500

    add stack to SymbolTable for returning back to previous scopes

--- Src/computer/SymbolTable.java
+++ Src/computer/SymbolTable.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.Stack;
 import computer.parsernodes.Types;
 
 public class SymbolTable {
@@ -18,9 +19,9 @@ public class SymbolTable {
 
     // symbol table gets built at parse, then is readonly during execution
     boolean completed;
-    String currentScope;
     HashMap<String, FunctionInfo> functionInfosByName;
     HashMap<String, HashMap<String, VariableInfo>> variableInfoByNameByFuncName;
+    Stack<String> programStack;
 
     public static SymbolTable instance() {
         assert instance != null;
@@ -31,13 +32,13 @@ public class SymbolTable {
         assert instance == null; // only one instance allowed :)
         instance = this;
         completed = false;
-        currentScope = null;
+        programStack = new Stack<String>();
         functionInfosByName = new HashMap<String, FunctionInfo>();
         variableInfoByNameByFuncName = new HashMap<String, HashMap<String, VariableInfo>>();
     }
 
     public String currentScope() {
-        return currentScope;
+        return programStack.peek();
     }
 
     /// Returns a copy of the current scope's info
@@ -80,60 +81,57 @@ public class SymbolTable {
     public boolean isVariableInitialized(String variableName)
     {
         assert variableInfoByNameByFuncName.get(currentScope()).containsKey(variableName);
+        System.out.println("checking if " + variableName + " is initialized");
         return variableInfoByNameByFuncName.get(currentScope()).get(variableName).isInitialized();
     }
 
     /// For use after symbol table has been built
     public void enterScope(String functionName) {
-        // no nested scopes in jott. exitScope() required, although technically
-        // we could just allow enterScope to be called and it just overwrite the
-        // previous scope
-        assert currentScope == null; 
         assert completed;
         assert variableInfoByNameByFuncName.containsKey(functionName);
         assert functionInfosByName.containsKey(functionName);
-        currentScope = functionName;
+        programStack.push(functionName);
     }
 
     /// For use after the symbol table has been built
     public void exitScope() {
-        assert currentScope != null;
+        assert !programStack.empty();
         assert completed;
 
         // Clear all vars
-        HashMap<String, VariableInfo> variableInfoByName = variableInfoByNameByFuncName.get(currentScope);
+        HashMap<String, VariableInfo> variableInfoByName = variableInfoByNameByFuncName.get(currentScope());
         for (String varName : variableInfoByName.keySet())
         {
             variableInfoByName.get(varName).markUninitialized();
         }
-        
-        currentScope = null;
+
+        programStack.pop();
     }
 
     public void beginBuildingNewScope(String functionName, Types returnType, ArrayList<Types> parameterTypes,
                     ArrayList<String> parameterNames) {
         assert !completed;
-        assert currentScope == null;
+        assert programStack.empty();
         assert !variableInfoByNameByFuncName.containsKey(functionName);
         assert !functionInfosByName.containsKey(functionName);
         HashMap<String, VariableInfo> scope = new HashMap<String, VariableInfo>();
         variableInfoByNameByFuncName.put(functionName, scope);
         functionInfosByName.put(functionName, new FunctionInfo(returnType, parameterTypes, parameterNames));
-        currentScope = functionName;
+        programStack.push(functionName);
     }
 
     /// Only valid to call while building symbol table, before execution
     public void finishBuildingCurrentScope(FuncBodyNode linkBodyNode) {
-        functionInfosByName.get(this.currentScope).setFuncBody(linkBodyNode);
+        functionInfosByName.get(currentScope()).setFuncBody(linkBodyNode);
         assert !completed;
-        assert currentScope != null;
-        currentScope = null;
+        assert programStack.size() == 1;
+        programStack.pop();
     }
 
     /// Only valid to call while building symbol table, before execution
     public void addVariableToCurrentScope(String variableName, Types type) {
         assert !completed;
-        HashMap<String, VariableInfo> variableInfoByName = variableInfoByNameByFuncName.get(currentScope);
+        HashMap<String, VariableInfo> variableInfoByName = variableInfoByNameByFuncName.get(currentScope());
         assert !variableInfoByName.containsKey(variableName);
         variableInfoByName.put(variableName, new VariableInfo(type));
     }
@@ -145,7 +143,7 @@ public class SymbolTable {
     /// always valid to call regardless of finalization.
     public void finalizeTable() {
         assert !completed;
-        assert currentScope == null;
+        assert programStack.empty();
         completed = true;
     }
 
